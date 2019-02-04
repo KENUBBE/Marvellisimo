dd786c3ab475674ea59c68f4547bbb8e1e812e0a
John Gherga <John4k25@gmail.com>
2019-02-04 18:41:02 +0100

    Created favorite button on character info view, Add character to favorite in db on click, Delete if clicked again.

--- app/src/main/java/com/marvellisimo/character/InfoActivity.kt
+++ app/src/main/java/com/marvellisimo/character/InfoActivity.kt
@@ -2,9 +2,9 @@ package com.marvellisimo.character
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.CheckBox
 import android.widget.CompoundButton
+import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import com.marvellisimo.R
 import com.marvellisimo.dto.character.Character
@@ -15,38 +15,53 @@ class InfoActivity : AppCompatActivity(), CompoundButton.OnCheckedChangeListener
 
     lateinit var character: Character
     lateinit var db: FirebaseFirestore
+    lateinit var fav: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
         db = FirebaseFirestore.getInstance()
         character = intent.getParcelableExtra("char")
-        val fav: CheckBox = findViewById(R.id.fav_char)
-
+        fav = findViewById(R.id.fav_char)
         fav.setOnCheckedChangeListener(this)
 
         infoName.text = character.name
         if (character.description != "") infoDesc.text = character.description else infoDesc.text = getString(R.string.no_char_description)
 
         Picasso.get().load(character.thumbnail.createUrl()).fit().centerCrop().into(infoThumbnail)
+        isCharacterInDB()
     }
 
 
     override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
-            if(isChecked) {
-                
-                // Add a new document with a generated ID
-                /*db.collection("favoriteCharacters")
-                    .add(character)
-                    .addOnSuccessListener { documentReference ->
-                        Log.d("favoriteCharacters", "DocumentSnapshot added with ID: " + documentReference.id)
-                    }
-                    .addOnFailureListener { e ->
-                        Log.w("favoriteCharacters", "Error adding document", e)
-                    }*/
-            } else {
+        if(isChecked) {
+            isCharacterInDB()
+        } else {
+            deleteCharacterFromFavorite()
+        }
+    }
 
-                println("Not added")
+    private fun addCharacterToFavorite() {
+            db.collection("favoriteCharacters").document(character.id.toString())
+                .set(character)
+            Toast.makeText(this, "Added to favorites", Toast.LENGTH_LONG).show()
+    }
+
+    private fun deleteCharacterFromFavorite() {
+        db.collection("favoriteCharacters").document(character.id.toString())
+            .delete()
+        Toast.makeText(this, "Deleted from favorites", Toast.LENGTH_LONG).show()
+    }
+
+    private fun isCharacterInDB() {
+        db.collection("favoriteCharacters").document(character.id.toString())
+            .get()
+            .addOnSuccessListener { result ->
+                if (result.exists()) {
+                    fav.isChecked = true
+                } else if (!result.exists() && fav.isChecked){
+                    addCharacterToFavorite()
+                }
             }
     }
 }
