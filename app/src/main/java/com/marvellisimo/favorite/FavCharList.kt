72d93071b5c9e64670a72757b1edca202ab80caf
KENUBBE <akas@live.se>
2019-02-06 15:23:33 +0100

    rendering fav char list from firestore

--- app/src/main/java/com/marvellisimo/favorite/FavCharList.kt
+++ app/src/main/java/com/marvellisimo/favorite/FavCharList.kt
@@ -5,11 +5,14 @@ import android.os.Bundle
 import com.google.firebase.firestore.FirebaseFirestore
 import com.marvellisimo.R
 import com.marvellisimo.dto.character.Character
+import com.marvellisimo.dto.character.Thumbnail
+import kotlinx.android.synthetic.main.activity_fav_char_list.*
+import java.lang.StringBuilder
 
 class FavCharList : AppCompatActivity() {
 
     private val db = FirebaseFirestore.getInstance()
-    private val characters = arrayListOf<Character>()
+    private val favCharacters = arrayListOf<Character>()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,24 +21,18 @@ class FavCharList : AppCompatActivity() {
         loadFavoriteCharacter()
     }
 
-
     private fun loadFavoriteCharacter() {
         db.collection("favoriteCharacters")
             .get()
             .addOnSuccessListener { result ->
                 for (document in result) {
-
-                    val asd = document.data.values
-
-                    /*val char = Character(
-                        Integer.parseInt(document.get("id").toString()),
-                        document.get("name").toString(),
-                        document.get("description").toString(),
-                        document.data.getValue("series"),
-                        document.get("thumbnail") as Thumbnail
-                    )*/
+                    val char = document.toObject(Character::class.java)
+                    favCharacters.add(char)
                 }
-                println("CHARACTERS $characters")
+                println("CHARACTERS ${favCharacters.size}")
+                favCharTv.text =
+                    "${favCharacters[0].name} \n ${favCharacters[1].name} \n ${favCharacters[2].name} \n ${favCharacters[3].name}"
             }
     }
+
 }
