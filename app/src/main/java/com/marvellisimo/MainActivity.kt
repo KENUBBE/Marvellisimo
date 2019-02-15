f781174f97e85b3033ec7d90ba6ff6a0f34257c4
John Gherga <John4k25@gmail.com>
2019-02-15 19:43:39 +0100

    Created login and create account functions using firebase auth. Also added online/offline status to users, status changes on sign in/ sign out/ app in background / app in foreground

--- app/src/main/java/com/marvellisimo/MainActivity.kt
+++ app/src/main/java/com/marvellisimo/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.marvellisimo
 
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+import android.arch.lifecycle.ProcessLifecycleOwner
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.ImageView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.FirebaseFirestore
 import com.marvellisimo.service.HexBuilder
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    val marvelService = HexBuilder()
+class MainActivity : AppCompatActivity(), LifecycleObserver {
+    private val marvelService = HexBuilder()
+    private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
         setSupportActionBar(toolbar_main)
         marvelService.generateHashKey()
 
@@ -21,8 +31,29 @@ class MainActivity : AppCompatActivity() {
         val backgroundImageView: ImageView = findViewById(R.id.main_background)
         backgroundImageView.setPadding(0, 150, 0,0)
         Picasso.get().load(R.drawable.marvel_main_background).fit().into(backgroundImageView)
-
-
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onAppBackgrounded() {
+        //App in background
+        setUserStatus(auth.currentUser, false)
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onAppForegrounded() {
+        // App in foreground
+        setUserStatus(auth.currentUser, true)
+    }
+
+    private fun setUserStatus(currentUser: FirebaseUser?, status: Boolean) {
+        val userRef = db.collection("users").document(currentUser?.uid.toString())
+
+        if (status) {
+            userRef
+                .update("status", "online")
+        } else {
+            userRef
+                .update("status", "offline")
+        }
+    }
 }
